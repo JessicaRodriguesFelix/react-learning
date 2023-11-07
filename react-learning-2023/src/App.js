f522378498bf00d5b3891f7ee2a8611d69c56127
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-11-07 13:27:27 -0500

    add useState for name and extract setCount to another function

--- react-learning-2023/src/App.js
+++ react-learning-2023/src/App.js
@@ -5,11 +5,26 @@ import "./App.css";
 /* useState takes one optional argument, which is the default state */
 function App() {
   const [count, setCount] = useState(0); //initial state in this case is zero, useState returns an array with 2 values: current state and set function that lets you update the state to a different value and triggers a re-render .
+  const [name, setName]= useState("Taylor");
   console.log("count initial state", count);
+
+  function handleClickName() {
+    setName("Robin");
+    console.log('name', name); // Still "Taylor"!
+  }
+  function handleClickCount() {
+    setCount(count + 1);
+  }
   return (
     <div align="center" marginTop="20px">
-      <button onClick={() => setCount(count + 1)}>Click Here</button>
-      <h1>{count}</h1>
+      <button onClick={handleClickCount}>Click Here</button>
+      {count === 0 ? (
+        <h1> You pressed me {count} time</h1>
+      ) : (
+        <h1> You pressed me {count} times</h1>
+      )}
+      <h1>{name}</h1>
+      <button onClick={handleClickName}>name</button>
     </div>
   );
 }
